31b445a027e3e12f2978b2d3ee05857b6a9f47b0
wawelk <wawelk@reynco.com>
2023-03-09 15:12:12 -0600

    QuickStart Lab

---
+++ ProjectFiles/NetSolution/AlarmGridLogic.cs
@@ -0,0 +1,44 @@
+#region Using directives
+using UAManagedCore;
+using FTOptix.NetLogic;
+using FTOptix.UI;
+using FTOptix.SQLiteStore;
+using FTOptix.Store;
+using FTOptix.EventLogger;
+using FTOptix.WebUI;
+using FTOptix.Recipe;
+using FTOptix.OPCUAServer;
+using FTOptix.DataLogger;
+#endregion
+
+public class AlarmGridLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        alarmsDataGridModel = Owner.Get<DataGrid>("AlarmsDataGrid").GetVariable("Model");
+
+        var currentSession = LogicObject.Context.Sessions.CurrentSessionInfo;
+        actualLanguageVariable = currentSession.SessionObject.Get<IUAVariable>("ActualLanguage");
+        actualLanguageVariable.VariableChange += OnSessionActualLanguageChange;
+    }
+
+    public override void Stop()
+    {
+        actualLanguageVariable.VariableChange -= OnSessionActualLanguageChange;
+    }
+
+    public void OnSessionActualLanguageChange(object sender, VariableChangeEventArgs e)
+    {
+        var dynamicLink = alarmsDataGridModel.GetVariable("DynamicLink");
+        if (dynamicLink == null)
+            return;
+
+        // Restart the data bind on the data grid model variable to refresh data
+        string dynamicLinkValue = dynamicLink.Value;
+        dynamicLink.Value = string.Empty;
+        dynamicLink.Value = dynamicLinkValue;
+    }
+
+    private IUAVariable alarmsDataGridModel;
+    private IUAVariable actualLanguageVariable;
+}
